957a738697886aa54e157bdd3cd92a8dc949c56a
thapaswi48855 <thapsapps@gmail.com>
2024-10-19 18:19:03 +0530

    canvas

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'canvas_project'`, () => {
+  it(`should have as title 'weatherapp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('canvas_project');
+    expect(app.title).toEqual('weatherapp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('canvas_project app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('weatherapp app is running!');
   });
 });
